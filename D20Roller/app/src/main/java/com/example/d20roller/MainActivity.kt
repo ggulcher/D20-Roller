26f30e5485fb8b0b6676a5fb69420e9d70113cad
ggulcher <ggulcher1@gmail.com>
2020-10-13 16:25:15 -0400

    Sounds and performance issues
    Added dice roll and crit effects with mp3 files.
    
    Optimized code to make better use of resources.

--- D20Roller/app/src/main/java/com/example/d20roller/MainActivity.kt
+++ D20Roller/app/src/main/java/com/example/d20roller/MainActivity.kt
@@ -1,51 +1,61 @@
 package com.example.d20roller
 
+import android.media.MediaPlayer
 import android.os.Bundle
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity(), View.OnClickListener {
+open class MainActivity : AppCompatActivity(), View.OnClickListener {
 
-    // Default values saved globally
     private var numDice = 1
     private var amountBonus = 0
+    private var mediaPlayer: MediaPlayer? = MediaPlayer()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        // Run on startup
+
         selectDice()
         selectBonus()
     }
 
     private fun selectDice() {
-        // Default amount of dice is 1
-        numberDice.setText("${numDice}d")
-        // Govern increasing or decreasing the number of dice rolled
+        numberDice.text = "${numDice}d"
         diceAdd.setOnClickListener{
             numDice++
-            numberDice.setText("${numDice}d") }
+            numberDice.text = "${numDice}d"
+        }
         diceSubtract.setOnClickListener{
             numDice--
-            numberDice.setText("${numDice--}d") }
+            numberDice.text = "${numDice--}d"
+        }
     }
 
     private fun selectBonus() {
-        // Default roll bonus is 0
-        rollBonus.setText("${amountBonus}")
-        // Govern the amount of bonus added to dice roll
+        rollBonus.text = "${amountBonus}"
         bonusAdd.setOnClickListener{
             amountBonus++
-            rollBonus.setText("${amountBonus}") }
+            rollBonus.text = "${amountBonus}"
+        }
         bonusSubtract.setOnClickListener{
             amountBonus--
-            rollBonus.setText("${amountBonus}") }
+            rollBonus.text = "${amountBonus}"
+        }
+    }
+
+    private fun playSoundFile(fileName: Int) {
+        mediaPlayer = MediaPlayer.create(this, fileName)
+        mediaPlayer?.start()
+    }
+
+    private fun stopSoundFile() {
+        mediaPlayer?.release()
+        mediaPlayer = null
     }
 
     override fun onClick(v: View?) {
-        // Used to determine size of num range for selecting a number
-        var buttonValue = when (v?.id) {
+        val buttonValue = when (v?.id) {
             R.id.d4 -> 4
             R.id.d6 -> 6
             R.id.d8 -> 8
@@ -54,19 +64,22 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
             R.id.d20 -> 20
             else -> 100
         }
-        // Find final result, considering number of dice and amount of bonus
-        var dice = Dice(buttonValue)
-        var diceRoll = ((dice.roll() + amountBonus)*numDice)
-        // Display final result to TextView
-        rollResult.setText(diceRoll.toString())
-        // Pseudo code for sound file
-        // if (dice.roll() == 20) { play "triumph" }
+        val dice = Dice(buttonValue)
+        val diceRoll = ((dice.roll() + amountBonus)*numDice)
+
+        rollResult.text = diceRoll.toString()
+        if (buttonValue == 20 && diceRoll == 20) {
+            playSoundFile(R.raw.fanfare)
+        } else {
+            playSoundFile(R.raw.diceroll)
+        }
+        mediaPlayer?.setOnCompletionListener { stopSoundFile() }
     }
 }
 
-// Separate class for generation of random number used by all dice buttons
 class Dice(private var numSides: Int) {
     fun roll(): Int {
         return (1..numSides).random()
     }
 }
+
